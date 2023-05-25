e4abb1d163a543be52971aa4affdfae539d1b3cc
Vasil Ivanov <ivanovvasil99@gmail.com>
2023-05-25 20:58:35 +0300

    Update rebuild script to use globs

--- rebuild_dev.sh
+++ rebuild_dev.sh
@@ -3,11 +3,11 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 cd $REPO_ROOT
-rm ${REPO_ROOT}/bunch1s.cpython-311-x86_64-linux-gnu.so || true
+find . -name "bunch1s.*.so" -delete
 rm -rf ${REPO_ROOT}/build || true
 mkdir build 
 cmake -S . -B build 
 cd build
 make
 cd $REPO_ROOT 
-cp build/bunch1s.cpython-311-x86_64-linux-gnu.so ${REPO_ROOT}
+cp build/bunch1s.*.so ${REPO_ROOT}
